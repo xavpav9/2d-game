085db719f2003e49b86026f64c64bee24815e86c
Xavier Tondelier <xtondelier@gmail.com>
2026-02-26 15:11:02 +0000

    Add message sending and receiving
    Uses a header to say the number of bytes being sent, then sends the
    data.

--- client.py
+++ client.py
@@ -1,18 +1,41 @@
 import socket
 
 ip = "127.0.0.1"
-port = 2000
+port = 2001
 
 class Client:
     def __init__(self, ip, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((ip, port))
+        self.headersize = int(self.sock.recv(8).decode(encoding="utf-8"))
+
+    def sendData(self, data):
+        encodedData = data.encode(encoding="utf-8")
+        dataLength = len(encodedData)
+        formattedData = f"{dataLength:<{self.headersize}}".encode(encoding="utf-8") + encodedData
+        self.sock.send(formattedData)
+
+    def recvData(self):
+        dataLength = int(self.sock.recv(self.headersize).decode(encoding="utf-8"))
+        data = ""
+        for i in range(dataLength // 8):
+            newData = self.sock.recv(8)
+            if newData == b"": return ""
+            data += newData.decode(encoding="utf-8")
+        if dataLength % 8 != 0:
+            newData = self.sock.recv(dataLength % 8)
+            data += newData.decode(encoding="utf-8")
+
+        return data
     
-    def closeConnection(self):
+    def close(self):
         self.sock.shutdown(socket.SHUT_RDWR)
         self.sock.close()
 
 
 client = Client(ip, port)
 
-client.closeConnection()
+print(client.recvData())
+client.sendData("hello back")
+
+client.close()
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import socket
 
 ip = "0.0.0.0"
-port = 2000
+port = 2001
 
 class Server:
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, headersize):
+        self.headersize = headersize
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((ip, port))
@@ -14,8 +15,28 @@ class Server:
         self.conns = []
         self.conn_names = [self.sock]
 
+    def sendData(self, conn, data):
+        encodedData = data.encode(encoding="utf-8")
+        dataLength = len(encodedData)
+        formattedData = f"{dataLength:<{self.headersize}}".encode(encoding="utf-8") + encodedData
+        conn.send(formattedData)
+
+    def recvData(self, conn):
+        dataLength = int(conn.recv(self.headersize).decode(encoding="utf-8"))
+        data = ""
+        for i in range(dataLength // 8):
+            newData = conn.recv(8)
+            if newData == b"": return ""
+            data += newData.decode(encoding="utf-8")
+        if dataLength % 8 != 0:
+            newData = conn.recv(dataLength % 8)
+            data += newData.decode(encoding="utf-8")
+
+        return data
+
     def acceptConnection(self):
         conn, addr = self.sock.accept()
+        conn.send(str(self.headersize).encode(encoding="utf-8"))
         self.conns.append({"conn": conn, "addr": addr})
         self.conn_names.append(conn)
 
@@ -32,8 +53,18 @@ class Server:
                 self.conn_names.pop(i+1)
                 break
 
+    def close(self):
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
 
-server = Server(ip, port)
+
+
+server = Server(ip, port, 8)
 
 conn = server.acceptConnection()
+
+server.sendData(conn, "hello")
+print(server.recvData(conn))
 server.removeConnection(conn)
+
+server.close()
